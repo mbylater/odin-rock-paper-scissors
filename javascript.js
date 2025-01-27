9f2d941369716fef1e112669a39aaf4cc3473e77
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-01-26 21:09:02 -0500

    complete step 5

--- javascript.js
+++ javascript.js
@@ -1,25 +1,3 @@
-// // When a user inputs a number
-// console.log(answer);
-// // Loop from 1 to the entered number
-// If the current number is divisible by 3 then print "Fizz"
-// If the current number is divisible by 5 then print "Buzz"
-// If the current number is divisible by 3 and 5 then print "FizzBuzz"
-// Otherwise print the current number
-// let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-// for (let i = 1; i <= answer; i++){
-//     if (i % 3 === 0 && i % 5 === 0) {
-//         console.log("FizzBuzz");
-//     } else if (i % 3 == 0) {
-//         console.log("Fizz")
-//     } else if (i % 5 == 0) {
-//         console.log("Buzz")
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-//---
-
 // Create a new function named getComputerChoice, that generates random integer between two numbers (inlusive of the numbers)
 function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
@@ -28,35 +6,70 @@ function getRandomIntInclusive(min, max) {
 }
 
 //Call the function
-let randomChoice = getRandomIntInclusive(1, 3);
+let computerChoice = getRandomIntInclusive(1, 3);
 
 // Check randomChoice
 //console.log(randomChoice);
 
 // Log in console result depending on the generated random number
-if (randomChoice == 1) {
-    console.log('rock')
-} else if (randomChoice == 2) {
-    console.log('paper')
+if (computerChoice === 1) {
+    console.log('Computer choice is rock')
+} else if (computerChoice === 2) {
+    console.log('Computer choice is paper')
 } else {
-    console.log('scissors')
+    console.log('Computer choice is scissors')
 }
 
 //Create a new function named getHumanChoice.
 function getHumanChoice() {
-    let sign = prompt("Lwt's play! Type \"rock\", \"paper\", or \"scissors\"");
-    if (sign.toLowerCase() === "rock") {
-        return "rock";
-    } else if (sign.toLowerCase() === "paper") {
-        return "paper";
-    } else if (sign.toLowerCase() === "scissors") {
-        return "scissors";
+    let sign = prompt('Let\'s play! Type \"rock\", \"paper\", or \"scissors\"');
+    if (sign.toUpperCase() === 'ROCK') {
+        return 'rock';
+    } else if (sign.toUpperCase() === 'PAPER') {
+        return 'paper';
+    } else if (sign.toUpperCase() === 'SCISSORS') {
+        return 'scissors';
     } else {
-        alert("Invalid choice! Please type \"rock\", \"paper\", or \"scissors\".");
-        return getHumanChoice(); // Recursively call the function to ask again
+        alert('Invalid choice! Please type \"rock\", \"paper\", or \"scissors\".');
+        return getHumanChoice(); //Recursively call the function to ask again
     }
 }
 
-// Store the result in a variable
+//Store the result in a variable
 let humanChoice = getHumanChoice();
 console.log('Your choice is ', humanChoice);
+
+// Create two new variables named humanScore and computerScore in the global scope.
+let humanScore = 0;
+let computerScore = 0;
+
+// Create a new function named playRound.
+// Define two parameters for playRound: humanChoice and computerChoice. 
+// Use these two parameters to take the human and computer choices as arguments.
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'ROCK', computerChoice === 'paper') {
+        computerScore++;
+        console.log('You loose! Paper beats Rock');
+    } else if (humanChoice === 'ROCK', computerChoice === 'scissoers') {
+        humanScore++;
+        console.log('You win! Rock beats Scissors');
+    } else if (humanChoice === 'PAPER', computerChoice === 'rock') {
+        humanScore++;
+        console.log('You win! Paper beats Rock');
+    } else if (humanChoice === 'PAPER', computerChoice === 'scissors') {
+        computerScore++;
+        console.log('You loose! Scissors beats Paper');
+    } else if (humanChoice === 'SCISSORS', computerChoice === 'paper') {
+        humanScore++;
+        console.log('You win! Scissors beats Paper');
+    } else if (humanChoice === 'SCISSORS', computerChoice === 'rock') {
+        computerScore++;
+        console.log('You loose! Rock beats Scissors');
+    } else {
+        computerScore++;
+        humanScore++;
+        console.log('It\'s a tie! You both win!');
+    }
+}
+
+playRound(humanChoice, computerChoice);
